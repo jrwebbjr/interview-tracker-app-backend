9e915879b6db5109552a627b95467120557cbfee
jrwebbjr <webbjr@zohomail.com>
2022-04-24 13:09:05 -0500

    set up jobs controller

---
+++ controllers/api/jobs.js
@@ -0,0 +1,26 @@
+const e = require('express');
+const Job = require('../../models/job');
+
+module.exports = {
+    index,
+    show
+};
+
+async function index(req, res) {
+    try{
+        const jobs = await Job.find({}).sort('date').exec();
+        jobs.sort((a, b) => a.date.sortOrder - b.date.sortOrder);
+        res.status(200).json(jobs);
+    }catch(e){
+        res.status(400).json({ msg: e.message });
+    }
+}
+
+async function show(req, res) {
+    try{
+        const job = await Job.findById(req.params.id);
+        res.status(200).json(job);
+    }catch(e){
+        res.status(400).json({ msg: e.message })
+    }
+}
