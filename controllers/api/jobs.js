264b17445e6197ae3f58a93ed83dc514edc39a14
jrwebbjr <webbjr@zohomail.com>
2022-04-24 13:38:20 -0500

    adding induces routes needed on jobs TODO: edit, delete, update. Setup

--- controllers/api/jobs.js
+++ controllers/api/jobs.js
@@ -5,7 +5,7 @@ module.exports = {
     index,
     show
 };
-
+//index
 async function index(req, res) {
     try{
         const jobs = await Job.find({}).sort('date').exec();
@@ -15,7 +15,25 @@ async function index(req, res) {
         res.status(400).json({ msg: e.message });
     }
 }
+//new
+// async function new(req, res) {
+//     try{
+//         const jobs = await Job.create({}).exec();
+//         res.status(200).json(jobs);
+//     }catch(e){
+//         res.status(400).json({ msg: e.message });
+//     }
+// }
 
+//delete
+
+//update
+
+//create
+
+//edit
+
+//show
 async function show(req, res) {
     try{
         const job = await Job.findById(req.params.id);
