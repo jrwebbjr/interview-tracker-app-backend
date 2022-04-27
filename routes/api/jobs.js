051b6c259e1ec17a89814c5e6a3545883e24c67d
jrwebbjr <webbjr@zohomail.com>
2022-04-26 20:08:33 -0500

    updated jobs api route to get index

--- routes/api/jobs.js
+++ routes/api/jobs.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const jobsCtrl = require('../../controllers/api/jobs');
 
-router.get('/', jobsCtrl.index);
+// router.get('/', jobsCtrl.index);
 
 router.get('/:id', jobsCtrl.show);
 
 router.post('/', jobsCtrl.create);
+
+router.get('/', jobsCtrl.index);
 
 module.exports = router;
