4ed6790344279ee08c93d80b69eac9b5e9edab21
jrwebbjr <webbjr@zohomail.com>
2022-05-04 18:05:06 -0500

    realized i put routes in my user utilities, deleted them from utilities

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,52 +1,15 @@
-const User = require('../../models/User');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+// /routes/api/users.js
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-module.exports = {
-  create,
-  login,
-  checkToken
-};
+// POST /api/users
+router.post('/', usersCtrl.create);
+// POST /api/users/login
+router.post('/login', usersCtrl.login);
 
-function checkToken(req, res) {
-  console.log('req.user', req.user);
-  res.status(200).json(req.exp);
-}
+// GET /api/users/check-token
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
-async function login(req, res) {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) throw new Error();
-    const match = await bcrypt.compare(req.body.password, user.password);
-    if (!match) throw new Error();
-    res.status(200).json( createJWT(user) );
-  } catch {
-    res.status(400).json('Bad Credentials');
-  }
-}
-
-async function create(req, res) {
-  try {
-    const user = await User.create(req.body);
-    // token will be a string
-    const token = createJWT(user);
-    // send back the token as a string
-    // which we need to account for
-    // in the client
-    res.status(200).json(token);
-  } catch (e) {
-    res.status(400).json(e);
-  }
-}
-
-
-/*-- Helper Functions --*/
-
-function createJWT(user) {
-  return jwt.sign(
-    // data payload
-    { user },
-    process.env.SECRET,
-    { expiresIn: '24h' }
-  );
-}
+module.exports = router;
