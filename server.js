efb01db5180e381409bbd91ef859b4d219f63e59
jrwebbjr <webbjr@zohomail.com>
2022-07-13 21:03:42 -0500

    updated catch all route in server.js for deploy

--- server.js
+++ server.js
@@ -35,17 +35,26 @@ app.use('/api/jobs', require('./routes/api/jobs'));
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
 
-// This was commented out testing it 7/2/22
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// Commented out 7/13/22 after 504 error
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
+
+// Added below code 7/13/22 for deployment
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
 
 // Test to ensure server is deployed on Heroku 7/2/22
-app.get('/', (req, res) => { res.send('Hello from Express!') });
+// app.get('/', (req, res) => { res.send('Hello from Express!') });
 
 
 app.listen(port, function() {
