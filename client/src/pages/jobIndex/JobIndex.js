8208fb4136cca28c61acf690ee23d9b0db0ab99e
jrwebbjr <webbjr@zohomail.com>
2022-08-08 08:18:28 -0500

    added svg loader to index

--- client/src/pages/jobIndex/JobIndex.js
+++ client/src/pages/jobIndex/JobIndex.js
@@ -21,7 +21,7 @@ const JobIndex = ({  jobs }) => {
             </motion.div>
             <main className='w-screen min-h-max flex justify-center py-20 scroll-smooth overscroll-none'>
                 <div className='grid-flow-row grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-10 md:grid-cols-3 md:gap-15 lg:grid-cols-4 lg:gap-20'>
-                    {
+                    { jobs ?
                         jobs && jobs.map((job) => {
                             return (
                                 <div className="w-full h-full">
@@ -52,6 +52,43 @@ const JobIndex = ({  jobs }) => {
                                 </div>
                             ) 
                         })
+                        : 
+                        <>
+                            <div className='w-screen h-screen flex justify-center items-center'>
+                                <svg width="120" height="30" viewBox="0 0 120 30" xmlns="http://www.w3.org/2000/svg" fill="#fff">
+                                    <circle cx="15" cy="15" r="15">
+                                        <animate attributeName="r" from="15" to="15"
+                                                begin="0s" dur="0.8s"
+                                                values="15;9;15" calcMode="linear"
+                                                repeatCount="indefinite" />
+                                        <animate attributeName="fill-opacity" from="1" to="1"
+                                                begin="0s" dur="0.8s"
+                                                values="1;.5;1" calcMode="linear"
+                                                repeatCount="indefinite" />
+                                    </circle>
+                                    <circle cx="60" cy="15" r="9" fill-opacity="0.3">
+                                        <animate attributeName="r" from="9" to="9"
+                                                begin="0s" dur="0.8s"
+                                                values="9;15;9" calcMode="linear"
+                                                repeatCount="indefinite" />
+                                        <animate attributeName="fill-opacity" from="0.5" to="0.5"
+                                                begin="0s" dur="0.8s"
+                                                values=".5;1;.5" calcMode="linear"
+                                                repeatCount="indefinite" />
+                                    </circle>
+                                    <circle cx="105" cy="15" r="15">
+                                        <animate attributeName="r" from="15" to="15"
+                                                begin="0s" dur="0.8s"
+                                                values="15;9;15" calcMode="linear"
+                                                repeatCount="indefinite" />
+                                        <animate attributeName="fill-opacity" from="1" to="1"
+                                                begin="0s" dur="0.8s"
+                                                values="1;.5;1" calcMode="linear"
+                                                repeatCount="indefinite" />
+                                    </circle>
+                                </svg>
+                            </div>
+                        </>
                     } 
                 </div>
             </main>
